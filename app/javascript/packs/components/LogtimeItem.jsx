0badcfaaf223a5d4a3a2d1461b35eb799d28219a
Amy <amytang77@gmail.com>
2020-08-10 18:28:16 -0400

    cleaning up some console errors

--- app/javascript/packs/components/LogtimeItem.jsx
+++ app/javascript/packs/components/LogtimeItem.jsx
@@ -14,7 +14,7 @@ const LogtimeItem = ({ logtimeItems }) => {
               <td>{logtimeItem.totalBreakDuration}</td>
               <td>{logtimeItem.totalLogDuration}</td>
               <td className="text-right">
-                <button className="btn" title="Edit">
+                <span className="gutter" title="Edit">
                   <svg
                     width="1em"
                     height="1em"
@@ -32,8 +32,8 @@ const LogtimeItem = ({ logtimeItems }) => {
                       d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 0 0 .5.5H4v.5a.5.5 0 0 0 .5.5H5v.5a.5.5 0 0 0 .5.5H6v-1.5a.5.5 0 0 0-.5-.5H5v-.5a.5.5 0 0 0-.5-.5H3z"
                     />
                   </svg>
-                </button>
-                <button className="btn" title="Delete">
+                </span>
+                <span className="gutter" title="Delete">
                   <svg
                     width="1em"
                     height="1em"
@@ -48,7 +48,7 @@ const LogtimeItem = ({ logtimeItems }) => {
                       d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
                     />
                   </svg>
-                </button>
+                </span>
               </td>
             </tr>
           );
