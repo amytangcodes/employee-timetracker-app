6fe8c8b738fd09edb853fa98dd208950c9d0d303
Amy <amytang77@gmail.com>
2020-08-10 15:19:41 -0400

    add show and create methods for logtime items

--- app/controllers/api/v1/logtime_items_controller.rb
+++ app/controllers/api/v1/logtime_items_controller.rb
@@ -1,14 +1,34 @@
 class Api::V1::LogtimeItemsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_logtime_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @logtime_items = LogtimeItem.all
+    @logtime_items = current_user.logtime_items.all
   end
 
   def show
+    if authorized?
+      respond_to do |format|
+        format.json { render :show }
+      end
+    else
+      handle_unauthorized
+    end
   end
 
   def create
+    @logtime_item = current_user.logtime_items.build(logtime_item_params)
+    if authorized?
+      respond_to do |format|
+        if @logtime_item.save
+          format.json { render :show, status: :created, location: api_v1_logtime_item_path(@logtime_item)}
+        else
+          format.json { render json: @logtime_item.errors, status: :unprocessable_entity }
+        end
+      end
+    else
+      handle_unauthorized
+    end
   end
 
   def update
@@ -21,6 +41,22 @@ class Api::V1::LogtimeItemsController < ApplicationController
 
   def set_logtime_item
     @logtime_item = LogtimeItem.find(params[:id])
+  end
+
+  def authorized?
+    @logtime_item.user == current_user
+  end
+
+  def handle_unauthorized
+    unless authorized?
+      respond_to do |format|
+        format.json { render :unauthorized, status: 401 }
+      end
+    end
+  end
+
+  def logtime_item_params
+    params.require(:logtime_item).permit(:date, :clockIn, :clockOut, :totalBreakDuration)
   end
 
 end
